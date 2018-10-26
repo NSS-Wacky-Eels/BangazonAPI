e506fde7b02f5ed648df8dd1b73cd808aee1acb0
streatorward <streatorward@gmail.com>
2018-10-26 14:43:39 -0500

    made my controller and shit

---
+++ Bangazon/Controllers/PaymentTypesController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using StudentExercisesAPI.Data;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+
+namespace Bangazon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTypesController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public PaymentTypesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
