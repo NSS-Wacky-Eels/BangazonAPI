97525471559c16b59c4f73b38e29ba21ec05a2dc
Taylor Gulley <taylorlgulley@yahoo.com>
2018-10-29 11:22:39 -0500

    modified the csproj for Dapper and the Model/Controller for Order

--- Bangazon/Models/Order.cs
+++ Bangazon/Models/Order.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public Customer customer { get; set; }
+        public PaymentType paymentType { get; set; }
+    }
+}
