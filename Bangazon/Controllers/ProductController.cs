4d7195aa849accb3baf13d7f7a91684af7861433
Matthew Hall <mateusvanhalen@gmail.com>
2018-10-29 15:02:35 -0500

    gets working in postman

--- Bangazon/Controllers/ProductController.cs
+++ Bangazon/Controllers/ProductController.cs
@@ -32,31 +32,32 @@ namespace Bangazon.Controllers
         }
 
         // GET api/students?q=Taco
-        [HttpGet]
-        public async Task<IActionResult> Get(string q)
-        {
-            string sql = @"
-            SELECT
-                p.Id,
-                p.Title,
-                p.Price,
-                p.Description,
-                p.Quantity,
-                p.CustomerId,
-                p.ProductTypeId,
-                c.Id,
-                c.FirstName,
-                pt.Id,
-                pt.Name
+        //[HttpGet]
+        //public async Task<IActionResult> Get(string q)
+        //{
+        //    string sql = @"
+        //    SELECT
+        //        p.Id,
+        //        p.Title,
+        //        p.Price,
+        //        p.Description,
+        //        p.Quantity,
+        //        p.CustomerId,
+        //        p.ProductTypeId,
+        //        c.Id,
+        //        c.FirstName,
+        //        pt.Id,
+        //        pt.Name
                 
 
 
-            FROM Product p
-            JOIN Customer c ON p.CustomerId = c.Id
-            JOIN ProductType pt ON p.ProductTypeId = pt.Id
-            WHERE 1=1
-            ";
+        //    FROM Product p
+        //    JOIN Customer c ON p.CustomerId = c.Id
+        //    JOIN ProductType pt ON p.ProductTypeId = pt.Id
+        //    WHERE 1=1
+        //    ";
 
+            //STEVE STUFF RIGHT HERE
             //if (q != null)
             //{
             //    string isQ = $@"
@@ -67,38 +68,61 @@ namespace Bangazon.Controllers
             //    sql = $"{sql} {isQ}";
             //}
 
-            Console.WriteLine(sql);
+//        Console.WriteLine(sql);
 
-            using (IDbConnection conn = Connection)
-            {
-                //Properties in middle becuase Product is depenent on them (see ERD)
-                IEnumerable<Product> products = await conn.QueryAsync<Product, Customer, ProductType, Product>(
-                    sql,
-                    (product, customer, productType) =>
-                    {
-                        product.Customer = customer;
-                        product.ProductType = productType;
-                        return product;
-                    }
-                );
-                // USING ok here is a built in feature of c#. 
-                return Ok(products);
-            }
-        }
+//            using (IDbConnection conn = Connection)
+//            {
+//                //Properties in middle becuase Product is depenent on them (see ERD)
+//                IEnumerable<Product> products = await conn.QueryAsync<Product, Customer, ProductType, Product>(
+//                    sql,
+//                    (product, customer, productType) =>
+//                    {
+//                        // Customer and PrdocutType Models
+//                        product.Customer = customer;
+//                        product.ProductType = productType;
+//                        return product;
+//                    }
+//                );
+//                // USING OK here is a built in feature of c#. 
+//                return Ok(products);
+//}
+//        }
 
         // GET api/students/5
+
         [HttpGet("{id}", Name = "GetProduct")]
         public async Task<IActionResult> Get([FromRoute]int id)
         {
             string sql = $@"
             SELECT
-                s.Id,
-                s.FirstName,
-                s.LastName,
-                s.SlackHandle,
-                s.CohortId
-            FROM Student s
-            WHERE s.Id = {id}
+                p.Id,
+                p.Title,
+                p.Price,
+                p.Description,
+                p.Quantity
+            FROM Product p
+            WHERE p.Id = {id}
+            ";
+
+            using (IDbConnection conn = Connection)
+            {
+                IEnumerable<Product> products = await conn.QueryAsync<Product>(sql);
+                return Ok(products);
+            }
+        }
+        //here is where im gettin all
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string q)
+        {
+            string sql = @"
+            SELECT
+                p.Id,
+                p.Title,
+                p.Price,
+                p.Description,
+                p.Quantity
+            FROM Product p
             ";
 
             using (IDbConnection conn = Connection)
@@ -110,36 +134,37 @@ namespace Bangazon.Controllers
 
         // POST api/students
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] Student student)
+        public async Task<IActionResult> Post([FromBody] Product product)
         {
-            string sql = $@"INSERT INTO Student 
-            (FirstName, LastName, SlackHandle, CohortId)
+            string sql = $@"INSERT INTO Product 
+            (Title, Price, Description, Quantity)
             VALUES
             (
-                '{student.FirstName}'
-                ,'{student.LastName}'
-                ,'{student.SlackHandle}'
-                ,{student.CohortId}
+                '{product.Title}'
+                ,'{product.Price}'
+                ,'{product.Description}'
+                ,{product.Quantity}
             );
             SELECT SCOPE_IDENTITY();";
 
             using (IDbConnection conn = Connection)
             {
                 var newId = (await conn.QueryAsync<int>(sql)).Single();
-                student.Id = newId;
-                return CreatedAtRoute("GetStudent", new { id = newId }, student);
+                product.Id = newId;
+                return CreatedAtRoute("GetProduct", new { id = newId }, product);
             }
         }
 
         // PUT api/students/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromBody] Student student)
+        public async Task<IActionResult> Put(int id, [FromBody] Product product)
         {
             string sql = $@"
             UPDATE Student
-            SET FirstName = '{student.FirstName}',
-                LastName = '{student.LastName}',
-                SlackHandle = '{student.SlackHandle}'
+            SET Title = '{product.Title}',
+                Price = '{product.Price}',
+                Description = '{product.Description}'
+                Quantity = '{product.Quantity}'
             WHERE Id = {id}";
 
             try
@@ -156,7 +181,7 @@ namespace Bangazon.Controllers
             }
             catch (Exception)
             {
-                if (!StudentExists(id))
+                if (!ProductExists(id))
                 {
                     return NotFound();
                 }
@@ -171,7 +196,7 @@ namespace Bangazon.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            string sql = $@"DELETE FROM Student WHERE Id = {id}";
+            string sql = $@"DELETE FROM Product WHERE Id = {id}";
 
             using (IDbConnection conn = Connection)
             {
@@ -185,12 +210,12 @@ namespace Bangazon.Controllers
 
         }
 
-        private bool StudentExists(int id)
+        private bool ProductExists(int id)
         {
-            string sql = $"SELECT Id FROM Student WHERE Id = {id}";
+            string sql = $"SELECT Id FROM Product WHERE Id = {id}";
             using (IDbConnection conn = Connection)
             {
-                return conn.Query<Student>(sql).Count() > 0;
+                return conn.Query<Product>(sql).Count() > 0;
             }
         }
     }
--- Bangazon/Models/Product.cs
+++ Bangazon/Models/Product.cs
@@ -7,5 +7,10 @@ namespace Bangazon.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
     }
 }
