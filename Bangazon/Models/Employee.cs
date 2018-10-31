7a9586d5880e4f4e7f19c04357659ddd08c33689
Michael <mmcclenton16@gmail.com>
2018-10-31 16:11:41 -0500

    started training program. time 2 go

---
+++ Bangazon/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+
+//namespace Bangazon.Models
+//{
+//    public class Employee
+//    {
+//        public int Id { get; set; }
+//        public string FirstName { get; set; }
+//        public string LastName { get; set; }
+//        public int DepartmentId { get; set; }
+//        public bool IsSupervisor { get; set; }
+//        public Computer Computer { get; set; }
+
+//    }
+//}
